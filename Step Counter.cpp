68781deda3dbbdf2e9da31e751f2faee1f03e0ad
lol0x0lol <ivancunin2205@gmail.com>
2025-01-21 18:19:11 +0300

    Added file.

---
+++ Step Counter.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+class User {
+public:
+    User() = default;
+    explicit User(int amountSteps0, ageUser0) {
+        amountSteps = amountSteps0;
+        ageUser = ageUser0;
+    }
+private:
+    int amountSteps, ageUser;
+};
+
+int main()
+{
+    
+
+    system("pause");
+}
