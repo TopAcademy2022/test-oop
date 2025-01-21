5d649ae719cc403b2323b6be0d9325e743a67526
koektotoestya <antonbabinets24112008@gmail.com>
2025-01-21 18:20:10 +0300

    Task added

---
+++ Zadanie/dz.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+class window {
+private:
+	float length;
+	float width;
+public:
+	window(float l, float w) {
+		length = l;
+		width = w;
+	}
+	float get_lenght() {
+		return length;
+	}
+
+	float get_width() {
+		return width;
+	}
+
+	void set_width(float newW) {
+		width = newW;
+	}
+
+	void set_length(float newL) {
+		length = newL;
+	}
+};
+
+int main()
+{
+
+}
