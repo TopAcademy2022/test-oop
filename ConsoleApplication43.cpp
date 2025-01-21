e2c4b3291146d4259bf2a2949c42cbd6c91591c7
synjoke <azilka458@gmail.com>
2025-01-21 18:19:47 +0300

    thing

---
+++ ConsoleApplication43.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class quiz {
+public:
+
+
+private:
+	string question = "abc";
+	string answer = "def";
+};
+
+int main()
+{
+
+}
+
+
