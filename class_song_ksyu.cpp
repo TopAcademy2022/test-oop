fafa19df7e975508587711c8b721bf2de04a6e27
bbbgviff <ksyu.sl08@yandex.ru>
2025-01-21 18:21:06 +0300

    added class "Song"

---
+++ class_song_ksyu.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Song {
+private:
+	int song_length;
+	int size_file;
+public:
+	Song() = default;
+	explicit Song(int song_length1, int size_file1) {
+		song_length = song_length1;
+		size_file = size_file1;
+	}
+};
